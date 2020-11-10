4874f46dd7d5cbfa3879153399e9b14388e0e6d1
Zaka Salah Eddine <zakasalaheddine@gmail.com>
2020-11-10 18:30:10 +0100

    feat: add faq section

---
+++ components/faq-section.jsx
@@ -0,0 +1,49 @@
+export default function FAQSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start ?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+            veritatis?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+            veritatis?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Payment Methods ?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+            veritatis?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products do you offer.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+            veritatis?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import AboutSection from "../components/about-section";
-import ServicesSection from "../components/services-sections";
+import FAQSection from "../components/faq-section";
+import ServicesSection from "../components/services-section";
 
 export default function Home() {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FAQSection />
     </>
   );
 }
